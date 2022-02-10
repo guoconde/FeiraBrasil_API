cce5c5d4ce66d83a4f493caea3d7c3b0afc65a69
Bernardo Rodrigues <bernardo.rodrigues06@unifei.edu.br>
2022-02-09 22:10:21 -0300

    feat: add routes to check and delete cart products

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import authRouter from './authRouter.js';
+import cartRouter from './cartRouter.js';
 
 const router = Router();
 router.use(authRouter)
+router.use(cartRouter)
 
 export default router;
